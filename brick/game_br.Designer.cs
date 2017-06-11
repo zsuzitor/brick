705cdd47f0a853142fa2a57030a805a090665ea1
stas <avdosevstas@mail.ru>
2017-06-11 11:45:57 +0300

    vsegda otskakivaet vniz

---
+++ brick/game_br.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace brick
+{
+    partial class game_br
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.polosa_ = new System.Windows.Forms.PictureBox();
+            this.ball_ = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa_)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball_)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.polosa_);
+            this.panel1.Controls.Add(this.ball_);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 720);
+            this.panel1.TabIndex = 0;
+            // 
+            // polosa_
+            // 
+            this.polosa_.Image = global::brick.Properties.Resources.niz;
+            this.polosa_.Location = new System.Drawing.Point(444, 688);
+            this.polosa_.Name = "polosa_";
+            this.polosa_.Size = new System.Drawing.Size(150, 20);
+            this.polosa_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.polosa_.TabIndex = 1;
+            this.polosa_.TabStop = false;
+            // 
+            // ball_
+            // 
+            this.ball_.Image = global::brick.Properties.Resources.mach19;
+            this.ball_.Location = new System.Drawing.Point(313, 599);
+            this.ball_.Name = "ball_";
+            this.ball_.Size = new System.Drawing.Size(20, 20);
+            this.ball_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball_.TabIndex = 0;
+            this.ball_.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // game_br
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 720);
+            this.Controls.Add(this.panel1);
+            this.Name = "game_br";
+            this.Text = "game_br";
+            this.Load += new System.EventHandler(this.game_br_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.game_br_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.polosa_)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball_)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox polosa_;
+        private System.Windows.Forms.PictureBox ball_;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ brick/game_br.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace brick
+{
+    public partial class game_br : Form
+    {
+       // int[,] mass = new int[30, 24];
+        int speed_ball_left = 4;
+        int speed_ball_up=3;
+        public game_br()
+        {
+            InitializeComponent();
+        }
+
+        private void game_br_Load(object sender, EventArgs e)
+        {
+            new_game();
+        }
+
+        private void new_game()
+        {
+            Cursor.Hide();
+            for(int i=0;i<20;++i)
+            {
+                for(int i2=0;i2<30;++i2)
+                {
+                    PictureBox a = new PictureBox();
+                    a.SizeMode =PictureBoxSizeMode.StretchImage;
+                    a.Height = 30;
+                    a.Width = 30;
+                    a.Left = 30 * i2;
+                    a.Top = 30 * i;
+                    a.Image = Image.FromFile(@"br_img.jpg");
+                    panel1.Controls.Add(a);
+                    //mass[i2, i] = 1;
+
+                }
+            }
+                }
+        //тик
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ball_.Left += speed_ball_left;
+            ball_.Top += speed_ball_up;
+            if (Cursor.Position.X - 75 > panel1.Left && Cursor.Position.X + 75 < panel1.Right)
+                polosa_.Left = Cursor.Position.X - 75;
+            if (Cursor.Position.X - 75 < panel1.Left)
+                polosa_.Left = 0;
+            if (Cursor.Position.X + 75 > panel1.Right)
+                polosa_.Left = panel1.Right-150;
+          
+            if (ball_.Bottom>=polosa_.Top&&((ball_.Left<=polosa_.Right&&ball_.Left>=polosa_.Left)|| (ball_.Right <= polosa_.Right && ball_.Right >= polosa_.Left)))//
+            {
+                speed_ball_up = -Math.Abs(speed_ball_up);
+                if(Math.Abs( speed_ball_left)<5)
+                if (ball_.Left < polosa_.Left + 40|| ball_.Right > polosa_.Right - 40)
+                    speed_ball_left=speed_ball_left > 0 ? speed_ball_left + 1 : speed_ball_left - 1;
+                if (Math.Abs(speed_ball_left) > 0)
+                    if (ball_.Left > polosa_.Left + 40 && ball_.Left < polosa_.Right - 40)
+                    speed_ball_left = speed_ball_left > 0 ? speed_ball_left - 1 : speed_ball_left + 1;
+                if(Math.Abs(speed_ball_up)+ Math.Abs(speed_ball_left)!=7)
+                {
+                    if(speed_ball_up>0)
+                    {
+                        speed_ball_up = 7 - Math.Abs(speed_ball_left);
+                    }
+                    else
+                    {
+                        speed_ball_up = -(7 - Math.Abs(speed_ball_left));
+                    }
+                }
+            }
+            if(ball_.Left<=panel1.Left)
+            {
+                speed_ball_left = -speed_ball_left;
+            }
+            if(ball_.Right >= panel1.Right)
+            {
+                speed_ball_left = -speed_ball_left;
+            }
+            if (ball_.Top <= panel1.Top)
+            {
+                speed_ball_up = -speed_ball_up;
+            }
+            if(ball_.Bottom>= panel1.Bottom)
+                {
+                timer1.Enabled = false;
+                Cursor.Show();
+
+            }
+            if(panel1.Controls.Count==2)
+            {
+                timer1.Enabled = false;
+                Cursor.Show();
+                MessageBox.Show("u win");
+                this.Close();
+            }
+            else
+            foreach (var i in panel1.Controls)
+            {
+                
+                if (i is PictureBox)
+                {
+                    if (ball_.Top < ((PictureBox)i).Bottom&& ball_.Top > ((PictureBox)i).Top)
+                        if (((ball_.Left <= ((PictureBox)i).Right && ball_.Left >= ((PictureBox)i).Left) || (ball_.Right <= ((PictureBox)i).Right && ball_.Right >= ((PictureBox)i).Left)))
+                        {
+                            speed_ball_up =- speed_ball_up;
+                            ((PictureBox)i).Dispose();
+                        }
+
+                }
+                   
+
+            }
+
+        }
+       /* private void get_element_by_name(ref object s,string nn)
+        {
+            foreach(var i in panel1.Controls)
+            {
+                if()
+
+            }
+        }*/
+
+        private void game_br_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Left)
+            {
+                polosa_.Left -= 10;
+
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+
+                polosa_.Left += 10;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                Cursor.Show();
+                this.Close();
+                
+            }
+
+
+        }
+    }
+}
